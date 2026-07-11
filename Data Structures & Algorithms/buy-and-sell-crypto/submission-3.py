b5eacf8e4fded817c5a4b0ab1943d7f620dd9a06
Ayush Bhanot <ayushbhanot1010@gmail.com>
2026-07-11 19:49:54 -0400

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxP = 0
+
+        for i in range(len(prices)): #O(n) time
+            for j in range(i + 1, len(prices)): #O(n2) time
+                profit = prices[j] - prices[i]
+
+                maxP = max(profit, maxP)
+
+        return maxP
