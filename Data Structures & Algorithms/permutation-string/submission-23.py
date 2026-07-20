48e59ee5db3d8ff03f94318a58ef1f23750ab386
Ayush Bhanot <ayushbhanot1010@gmail.com>
2026-07-20 17:05:59 -0400

    Add: permutation-string - submission-23

---
+++ Data Structures & Algorithms/permutation-string/submission-23.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        if len(s1) > len(s2):
+            return False
+            
+        count1 = [0] * 26
+        for c in s1: #O(s1) time
+            count1[ord(c) - ord('a')] += 1
+        
+        l = 0
+        count2 = [0] * 26
+
+        for r in range(len(s2)): #O(s2) time
+            count2[ord(s2[r]) - ord('a')] += 1
+            if (r - l + 1) == len(s1):
+                if count2 == count1:
+                    return True
+                count2[ord(s2[l]) - ord('a')] -= 1
+                l += 1
+        return False #O(s2) time and O(1) space total?
