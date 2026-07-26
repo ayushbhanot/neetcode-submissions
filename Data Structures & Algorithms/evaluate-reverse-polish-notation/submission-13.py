4466661004857e0bdd1239c81ccc498120582eb6
Ayush Bhanot <ayushbhanot1010@gmail.com>
2026-07-26 16:43:51 -0400

    Add: evaluate-reverse-polish-notation - submission-13

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-13.py
@@ -0,0 +1,33 @@
+class DoublyLinkedList:
+    def __init__(self, val):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        operands = "-+/*"
+
+        curr = DoublyLinkedList(int(tokens[0]))
+        for i in range(1, len(tokens)): #O(n) time
+            if tokens[i] in operands:
+                if curr.prev:
+                    a = curr.prev.val
+                    b = curr.val
+                    if tokens[i] == "+":
+                        result = a + b
+                    elif tokens[i] == "-":
+                        result = a - b
+                    elif tokens[i] == "*":
+                        result = a * b
+                    elif tokens[i] == "/":
+                        result = int(a / b)
+                    curr = curr.prev
+                    curr.val = result
+                    continue
+            curr.next = DoublyLinkedList(int(tokens[i]))
+            tmp = curr
+            curr = curr.next
+            curr.prev = tmp
+
+        return curr.val
