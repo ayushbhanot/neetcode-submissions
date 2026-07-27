d4d16dcb17d2c370bb280e34e76b34ba2c473120
Ayush Bhanot <ayushbhanot1010@gmail.com>
2026-07-27 18:30:18 -0400

    Add: car-fleet - submission-11

---
+++ Data Structures & Algorithms/car-fleet/submission-11.py
@@ -0,0 +1,21 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        cars = []
+        for i in range(len(position)): #O(n) time
+            cars.append([position[i], speed[i]]) #O(n) space
+        
+        cars.sort(reverse = True) #O(nlogn) time and O(n) space due to Python's timsort
+
+
+        fleets = 0
+        prevTime = float("-inf")
+        for i in range(len(cars)): #O(n) time
+            distance = target - cars[i][0]
+            time = distance / cars[i][1]
+
+            if time > prevTime:
+                fleets += 1
+                prevTime = time
+
+        return fleets
