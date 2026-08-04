607fe1c8fea1125b12b5f5fefd396e30ecc8fa11
Ayush Bhanot <ayushbhanot1010@gmail.com>
2026-08-04 00:01:11 -0400

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 0, max(piles) + 1
+
+        while l < r:
+            m = l + (r - l) // 2
+
+            if self.isValid(m, h, piles):
+                r = m
+            else:
+                l = m + 1
+        return r
+
+
+    def isValid(self, speed, h, piles: List[int]) -> bool:
+
+        hours = 0
+
+        if speed == 0:
+            return False
+
+        for i in range(len(piles)): #O(n) time
+            if piles[i] / speed > piles[i] // speed:
+                hours += piles[i] // speed + 1
+                continue
+            hours += piles[i] // speed
+        
+        if hours <= h:
+            return True
+        else:
+            return False
