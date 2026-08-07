6389ced717ad9d9993a205c384bbe03c0c9c0b66
Ayush Bhanot <ayushbhanot1010@gmail.com>
2026-08-07 14:51:22 -0400

    Add: time-based-key-value-store - submission-11

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-11.py
@@ -0,0 +1,36 @@
+class TimeMap:
+
+    def __init__(self):
+        self.TimeMap = {} #O(1) time and space
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.TimeMap:
+            self.TimeMap[key] = []
+        self.TimeMap[key].append((timestamp, value)) #O(1) time and space
+
+    def get(self, key: str, timestamp: int) -> str:
+        
+        res = ""
+
+        if key not in self.TimeMap:
+            return res
+
+        history = self.TimeMap[key]
+
+        l, r = 0, len(history) - 1
+
+        while l <= r:
+
+            m = l + (r - l) // 2
+
+            if history[m][0] == timestamp:#(10, one),(20, two),(30, three)
+                res = history[m][1]
+                break
+
+            if history[m][0] > timestamp:
+                r = m - 1
+            elif history[m][0] < timestamp:
+                res = history[m][1]
+                l = m + 1
+
+        return res
