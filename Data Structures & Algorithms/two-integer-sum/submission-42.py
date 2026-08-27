aaf4a732a379faed640abb81b10f383b2ffaa7f8
Ayush Bhanot <ayushbhanot1010@gmail.com>
2026-08-27 09:47:37 -0400

    Add: two-integer-sum - submission-42

---
+++ Data Structures & Algorithms/two-integer-sum/submission-42.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)): #O(n) time
+            for j in range(i + 1, len(nums)): #O(n2) time
+                total = nums[i] + nums[j]
+                if total == target:
+                    return [i, j]
+
+        return []
