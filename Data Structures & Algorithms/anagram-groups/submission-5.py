c4a0b782b9cc226042203a6e5dd8eb7392e81e71
Ayush Bhanot <ayushbhanot1010@gmail.com>
2026-08-28 13:45:20 -0400

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        sortedMap = {}
+        for string in strs:
+            hashedString = self.hashString(string)
+            if hashedString not in sortedMap:
+                sortedMap[hashedString] = []
+            sortedMap[hashedString].append(string)
+
+        return list(sortedMap.values())
+
+    def hashString(self, string):
+        count = [0] * 26
+        for c in string:
+            count[ord(c) - ord('a')] += 1
+        return tuple(count)
