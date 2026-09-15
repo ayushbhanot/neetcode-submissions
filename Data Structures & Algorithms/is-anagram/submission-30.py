d1d3475f956f95bd4caecd6f3ef839347b2c12c7
Ayush Bhanot <ayushbhanot1010@gmail.com>
2026-09-15 12:14:14 -0400

    Add: is-anagram - submission-30

---
+++ Data Structures & Algorithms/is-anagram/submission-30.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        count = [0] * 26
+
+        if len(s) != len(t):
+            return False
+        
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+
+        for c in t:
+            count[ord(c) - ord('a')] -= 1
+            if count[ord(c) - ord('a')] < 0:
+                return False
+
+        for i in range(len(count)): #O(26) time => O(1) time
+            if count[i] != 0:
+                return False
+
+        return True # t = max(len(s), len(t)) worst case is O(t) time and O(1) space
+        
+        
